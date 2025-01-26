6b313af12475b0964702f5e5b079c7b18d78cdbf
douglasmatosdev <douglasmatosdev@gmail.com>
2025-01-26 13:54:54 -0300

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,323 @@
+'use client'
 import Image from "next/image";
+import { createRef, useState, useMemo } from "react";
+import styled from "styled-components";
+import Confetti from 'react-confetti'
 
+type Hero = {
+  battleIndex?: number;
+  player?: Player;
+  image: string;
+  name: string;
+  power: number;
+}
+
+type Player = 'left' | 'right'
+
+const deck = [
+  {
+    image: '/hero.jpeg',
+    name: 'john',
+    power: 3
+  },
+  {
+    image: '/hero.jpeg',
+    name: 'jane',
+    power: 5
+  },
+  {
+    image: '/hero.jpeg',
+    name: 'jim',
+    power: 2
+  },
+  {
+    image: '/hero.jpeg',
+    name: 'jill',
+    power: 4
+  },
+]
+
+const initialPlayers = { left: deck.slice(0, 2), right: deck.slice(2, 4) }
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [players, setPlayers] = useState<{ left: Hero[]; right: Hero[] }>(initialPlayers)
+  const [inBattle, setInBattle] = useState<Hero[]>([])
+  const [cemitery, setCemitery] = useState<Hero[]>([])
+  const [scores, setScores] = useState<{ left: number; right: number }>({ left: 0, right: 0 })
+  const [isExploding, setIsExploding] = useState(false);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+  const herosInBattleRef = useMemo(() => Array.from({ length: 2 }).fill(0).map(() => createRef<HTMLDivElement>()), []);
+
+  const handleClick = (player: Player, card: Hero) => {
+    if (inBattle.length < 2 && !inBattle.find((c) => player === c.player)) {
+      let newInBattle = [...inBattle, { player, ...card }]
+      newInBattle = newInBattle.map((c, index) => ({ ...c, battleIndex: index }))
+      setInBattle(newInBattle)
+      setPlayers({
+        ...players,
+        [player]: players[player].filter((c) => c.name !== card.name)
+      })
+
+      setTimeout(() => {
+        handleBattle(newInBattle)
+      }, 2000)
+    }
+  }
+
+  const handleBattle = (inBattle: Hero[]) => {
+    if (inBattle.length === 2) {
+      const leftHero = inBattle.find((c) => c.player === 'left')
+      const rightHero = inBattle.find((c) => c.player === 'right')
+
+      if (leftHero!.power > rightHero!.power) {
+        herosInBattleRef[leftHero!.battleIndex!].current?.classList.add('hero-winner')
+
+        setTimeout(() => {
+          setScores({
+            ...scores,
+            left: scores.left + 1
+          })
+
+          const updatedRight = players.right.filter((c) => c.name !== rightHero?.name)
+          const updatedLeft = [...players.left, leftHero!].filter((v, i, a) => a.findIndex(t => t.name === v.name) === i)
+
+          setPlayers({
+            left: updatedLeft,
+            right: updatedRight
+          })
+
+          setCemitery([...cemitery, rightHero!])
+
+          if (updatedRight.length === 0) {
+            setTimeout(() => {
+              alert('PLAYER LEFT WINS')
+              setIsExploding(true)
+
+              setTimeout(() => {
+                setIsExploding(false)
+                alert('RESTARTING GAME')
+                setPlayers(initialPlayers)
+                setCemitery([])
+                setScores({ left: 0, right: 0 })
+                setInBattle([])
+              }, 5000)
+            }, 500)
+          }
+        }, 2000)
+      } else {
+        herosInBattleRef[rightHero!.battleIndex!].current?.classList.add('hero-winner')
+
+        setTimeout(() => {
+          setScores({
+            ...scores,
+            right: scores.right + 1
+          })
+
+          const updatedLeft = players.left.filter((c) => c.name !== leftHero?.name)
+          const updatedRight = [...players.right, rightHero!].filter((v, i, a) => a.findIndex(t => t.name === v.name) === i)
+
+          setPlayers({
+            left: updatedLeft,
+            right: updatedRight
+          })
+
+          setCemitery([...cemitery, leftHero!])
+
+          if (updatedLeft.length === 0) {
+            setTimeout(() => {
+              alert('PLAYER RIGHT WINS')
+              setIsExploding(true)
+
+              setTimeout(() => {
+                setIsExploding(false)
+                alert('RESTARTING GAME')
+                setPlayers(initialPlayers)
+                setCemitery([])
+                setScores({ left: 0, right: 0 })
+                setInBattle([])
+              }, 5000)
+            }, 500)
+          }
+
+        }, 2000)
+      }
+
+      setTimeout(() => {
+        setInBattle([])
+      }, 2000)
+    }
+  }
+
+  return (
+    <div>
+      {isExploding && <Confetti
+        width={document.documentElement.clientWidth}
+        height={document.documentElement.clientHeight}
+      />}
+      <Board>
+
+        <BattleField>
+          <CemiteryField>
+            <span>Cemitery</span>
+            <div>
+              {cemitery.map((card, index) => (
+                <Card className="in-cemitery" ref={herosInBattleRef[index]} key={'cemitery' + index + card.name} onClick={() => handleClick('left', card)}>
+                  <Image src={card.image} alt={card.name} width={50} height={100} />
+                  <p>{card.name}</p>
+                  <p>{card.power}</p>
+                </Card>
+              ))}
+            </div>
+          </CemiteryField>
+
+          <BattleFieldMatch>
+            {inBattle.map((card, index) => (
+              <Card ref={herosInBattleRef[index]} key={'battle' + index + card.name} onClick={() => handleClick('left', card)}>
+                <Image src={card.image} alt={card.name} width={200} height={200} />
+                <p>{card.name}</p>
+                <p>{card.power}</p>
+              </Card>
+            ))}
+          </BattleFieldMatch>
+        </BattleField>
+        <PlayersDecks>
+          <BoardSide >
+            <BoardSideArea>
+              <p>Player: LEFT</p>
+              <p>Score: {scores.left}</p>
+            </BoardSideArea>
+            <div>
+
+              {players.left.map((card, index) => (
+                <Card key={'players-left' + index + card.name} onClick={() => handleClick('left', card)}>
+                  <Image src={card.image} alt={card.name} width={200} height={200} />
+                  <p>{card.name}</p>
+                  <p>{card.power}</p>
+                </Card>
+              ))}
+            </div>
+          </BoardSide>
+          <BoardSide >
+            <BoardSideArea>
+              <p>Player: RIGHT</p>
+              <p>Score: {scores.right}</p>
+            </BoardSideArea>
+            <div>
+
+              {players.right.map((card, index) => (
+                <Card key={'playr-right' + index + card.name} onClick={() => handleClick('right', card)}>
+                  <Image src={card.image} alt={card.name} width={200} height={200} />
+                  <p>{card.name}</p>
+                  <p>{card.power}</p>
+                </Card>
+              ))}
+            </div>
+          </BoardSide>
+        </PlayersDecks>
+      </Board>
     </div>
+
   );
 }
+const BattleFieldMatch = styled.div`
+display: flex;
+gap: 16px;
+
+`
+
+const CemiteryField = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  height: 150px;
+
+  background: brown;
+  padding: 16px;
+
+  & > div {
+    display: flex;
+    gap: 16px;
+  }
+`
+
+const BoardSideArea = styled.div`
+  display: flex;
+`
+const BattleField = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  height: 500px;
+  background: red;
+  border-radius: 8px;
+  padding: 16px;
+`
+const PlayersDecks = styled.div`
+  display: flex;
+  gap: 16px;
+
+
+  width: 100%;
+  height: 300px;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+`
+
+
+const BoardSide = styled.div`
+    width: 50%;
+    background: blue;
+    border-radius: 8px;
+    padding: 16px;
+  & > div {
+
+    display: flex;
+    justify-content: center;
+    gap: 16px;
+  }
+`
+const Board = styled.div`
+padding: 16px;
+
+`
+const Card = styled.div`
+  cursor: pointer;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 16px;
+  border-radius: 8px;
+  background: #fff;
+
+  
+  width: 200px;
+  height: 250px;
+  
+  p {
+    color: #333;
+    font-size: 24px;
+    margin: 16px 0;
+    border: 1px solid #333;
+    width: 100%;
+    text-align: center;
+  }
+  
+  &.hero-winner {
+    transform: scale(1.5);
+  }
+
+  &.in-cemitery {
+    padding: 4px;
+    border-radius: 2px;
+    width: 50px;
+    height: 100px;
+
+    p {
+    color: #333;
+    font-size: 16px;
+    margin: 4px 0;
+  }
+  }
+`
